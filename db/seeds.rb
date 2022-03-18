791d2fb9684c7624665c41d39e54d3e704aea7d6
mrsaliceselezneva <poluvesov.artyom@gmail.com>
2022-03-18 12:51:45 +0400

    db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+create_next_questions = Create_next_question.create([{number: 1}])
+
+questions = Question.create([{ title: 'q1'}, { title: 'q2'}])
+
+Answer.create(body: 'a1', question: questions.first)
